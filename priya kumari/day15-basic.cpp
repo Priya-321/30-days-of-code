d3e79d7a27b7d1313618b2bb2dc3b8dd71d8f6a2
Priya32pro <99943095+Priya-321@users.noreply.github.com>
2022-05-10 10:51:10 +0530

    day15-basic.cpp

---
+++ priya kumari/day15-basic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+        int h,x;
+        cin>>h>>x;
+        if(h>=x)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+     }
+     
+	return 0;
+}
